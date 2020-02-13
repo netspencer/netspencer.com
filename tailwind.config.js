21f72c6b9fe0a3f1e584cb16422d7f3325082208
Spencer Schoeben <spencer@netspencer.com>
2020-02-13 00:02:36 -0800

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     extend: {
       fontSize: {
         '7xl': '5rem'
+      },
+      fontFamily: {
+        sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif]
       }
     }
   },
