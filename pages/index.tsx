cde3d04384f7374a8f0eab8a3ab7b83d845b9695
Spencer Schoeben <spencer@netspencer.com>
2024-08-15 12:54:07 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -151,6 +151,16 @@ const Page: NextPage = () => (
         </p>
       </main>
     </div>
+
+        <script dangerouslySetInnerHTML={{__html: `  window.delphi = {
+    config: "8bbf71fd-d47b-4705-a44f-28773432b8e2", // embed id
+    type: "page" // or "bubble"
+  };`}} />
+
+
+<script dangerouslySetInnerHTML={{__html: `!function(){var e=window,t=document,n=function(){if(!e.delphi||"page"!==e.delphi.type&&"bubble"!==e.delphi.type)throw new Error("Invalid or missing delphi type. Must be 'page' or 'bubble'.");var n=t.createElement("script");n.type="text/javascript",n.async=!0,n.defer=!0,n.src="page"===e.delphi.type?"https://embed.delphi.ai/bundle.js":"https://embed.delphi.ai/widget.js",e.delphi&&e.delphi.config&&n.setAttribute("data-config",e.delphi.config);var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)};"complete"===t.readyState?n():e.attachEvent?e.attachEvent("onload",n):e.addEventListener("load",n,!1)}();
+`}} />
+
   </>
 )
 
